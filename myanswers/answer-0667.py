45733df22cf6ad1e4d76aaf34c7ba4bc0fd1897f
Oliver Andres Quiroz Mora <oliver.quiroz@udea.edu.co>
2026-05-28 19:46:50 -0500

    Update answer-0667.py

--- myanswers/answer-0667.py
+++ myanswers/answer-0667.py
@@ -1,1 +1,6 @@
+import numpy as np
+from sklearn.preprocessing import Binarizer
 
+def clasificar_por_umbral_custom(y_probs, umbral):
+    binarizer = Binarizer(threshold=umbral)
+    return binarizer.transform(y_probs)
