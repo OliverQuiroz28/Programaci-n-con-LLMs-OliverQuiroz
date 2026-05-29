ad10f1aa5a0081ad01438daf84ad59576864bd49
Oliver Andres Quiroz Mora <oliver.quiroz@udea.edu.co>
2026-05-28 19:39:39 -0500

    Add function to filter and sort DataFrame

--- myanswers/answer-0493.py
+++ myanswers/answer-0493.py
@@ -1,1 +1,5 @@
+import pandas as pd
 
+def filtrar_y_ordenar(df, min_price, min_rating):
+    return df[(df["price"] > min_price) & (df["rating"] >= min_rating)]\
+              .sort_values(by="price", ascending=False)
